e8df2261136b474268997a1fb2b7553b0376784d
Dmitry Smolyakov <mompizzapls@mail.ru>
2018-08-30 19:03:53 +0300

    дополил процессор отправляющий собщения

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Поднять очереди:
 
+В .end установить ``ENQUEUE_DSN`` равный ``DATABASE_URL``
+
 ```Для посгреса: CREATE EXTENSION IF NOT EXISTS "uuid-ossp";```
 
 ```./bin/console enqueue:consume --setup-broker -vvv```
--- src/Process/SendContactInfoToAdminProcessor.php
+++ src/Process/SendContactInfoToAdminProcessor.php
@@ -12,9 +12,25 @@ use Enqueue\Client\TopicSubscriberInterface;
 
 class SendContactInfoToAdminProcessor implements PsrProcessor, TopicSubscriberInterface
 {
+    protected $mailer;
+
+    protected $adminEmail;
+
+    public function __construct(\Swift_Mailer $mailer, string $adminEmail)
+    {
+        $this->mailer = $mailer;
+        $this->adminEmail = $adminEmail;
+    }
+
     public function process(PsrMessage $message, PsrContext $session)
     {
-        //todo: sdelat
+        echo $message->getBody();
+
+        $this->mailer->send((new \Swift_Message('Hello Email'))
+            ->setFrom('no-reply@example.com')
+            ->setTo($this->adminEmail)
+            ->setBody($message->getBody())
+        );
 
         return self::ACK;
         // return self::REJECT; // when the message is broken
@@ -23,6 +39,6 @@ class SendContactInfoToAdminProcessor implements PsrProcessor, TopicSubscriberIn
 
     public static function getSubscribedTopics()
     {
-        return ['aFooTopic'];
+        return ['aSendContactInfoToAdmin'];
     }
 }
